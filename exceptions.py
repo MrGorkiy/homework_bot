d0dada8ba141caefc3a2946421ccde2b9b1c6702
MrGorkiy <brysin95@mail.ru>
2022-05-05 22:15:59 +0300

    Готовая работа прошедшая pytest

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+class TokenError(Exception):
+    """Класс исключений при отсутствии токена"""
+    pass
+
+
+class ApiError(Exception):
+    """Класс исключений некорректный в API ответе"""
+    pass
+
+
+class ParseNoneStatus(Exception):
+    pass
--- homework.py
+++ homework.py
@@ -1,83 +1,271 @@
-...
+import logging
+import os
+import sys
+import time
+from http import HTTPStatus
+from typing import Union
+
+import requests
+from telegram import Bot
+from dotenv import load_dotenv
+
+from exceptions import ApiError, TokenError, ParseNoneStatus
 
 load_dotenv()
 
+logging.basicConfig(
+    datefmt="%H:%M:%S",
+    filename="main.log",
+    encoding="UTF-8",
+    filemode="a",
+    level=logging.INFO,
+)
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(stream=sys.stdout)
+formatter = logging.Formatter(
+    "%(asctime)s [%(levelname)s] %(message)s (%(funcName)s | %(lineno)d)"
+)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
+RETRY_TIME = 10
+ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
+HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
 HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+    "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
+    "reviewing": "Работа взята на проверку ревьюером.",
+    "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
 
 
-def send_message(bot, message):
-    ...
+def send_message(bot: Bot, message: str):
+    """
+    Функция отправляет сообщения в Telegram чат
+
+    Отправляет сообщение в Telegram чат, определяемый
+    переменной окружения TELEGRAM_CHAT_ID
+
+    :param bot: экземпляр класса Bot
+    :type bot: telegram.Bot
+    :param message: Строка с текстом сообщения
+    :type message: str
+    """
+
+    bot.send_message(TELEGRAM_CHAT_ID, message)
+    logging.info("Сообщение успешно отправлено")
 
 
-def get_api_answer(current_timestamp):
+def get_api_answer(current_timestamp: int) -> dict:
+    """
+    Делает запрос к API-сервису
+
+    Функция get_api_answer() делает запрос к единственному эндпоинту
+    API-сервиса. В качестве параметра функция получает временную метку.
+
+    :param current_timestamp: unitime - временная метка
+    :type current_timestamp: int
+    :return: возвращает API запрос преобразовав его из формата JSON к
+    типам данных Python.
+    :rtype: dict
+
+    :raises ApiError: Возникает ошибка при ошибках обращения к API
+    """
+
     timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
+    params = {"from_date": timestamp}
+    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    if homework_statuses.status_code == HTTPStatus.OK:
+        return homework_statuses.json()
+    elif (homework_statuses.status_code == HTTPStatus.NOT_FOUND
+          and HTTPStatus.FOUND):
+        raise ApiError
+    else:
+        raise ApiError
 
-    ...
+
+def check_response(response) -> Union[bool, dict]:
+    """
+    Проверка API на корректность
+
+    Функция проверяет ответ API на корректность.
+    В качестве параметра функция получает ответ от API, в формате dict.
+    Если ответ API соответствует ожиданиям, то
+    функция возвращает список домашних работ (он может быть и пустым)
+
+    :param response: Ответ от API в формате dict
+    :type response: dict
+
+    :return: Возвратит список домашних работ.
+    :rtype: dict
+
+    :raises TokenError: Ошибка в случае некорректности ответа API
+    """
+
+    if not response['homeworks']:
+        return False
+    elif type(response["homeworks"]) != list:
+        raise TokenError
+    else:
+        return response["homeworks"]
 
 
-def check_response(response):
+def parse_status(homework: dict) -> Union[bool, str]:
+    """
+    Функция извлекает из информации о конкретной домашней
+    работе статус работы.
 
-    ...
+    :param homework: Один элемент из списка домашних работ.
+    :type homework: dict
 
+    :return: В случае корректности возвращается сообщение, иначе
+    булевое значение
+    :rtype: bool|str
 
-def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+    :raises ParseNoneStatus: Недокументированный статус домашней работы
+    """
 
-    ...
+    if homework:
+        homework_name = homework["homework_name"]
+        homework_status = homework["status"]
+    else:
+        return False
 
-    verdict = ...
-
-    ...
+    try:
+        verdict = HOMEWORK_STATUSES[homework_status]
+    except KeyError:
+        raise ParseNoneStatus
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def check_tokens():
-    ...
+def check_tokens() -> bool:
+    """
+    Функция проверяет доступность переменных с токенами в файле .evn
+    при его отсутствии, требуется создать, пример в .evn.example
+
+    :return: если все переменные возвращает True, иначе False
+    :rtype: bool
+
+    :raises TokenError: Отсутствие обязательной переменной
+    """
+
+    practicum_token_correct = False
+    telegram_token_correct = False
+    telegram_chat_id_correct = False
+
+    try:
+        if PRACTICUM_TOKEN:
+            practicum_token_correct = True
+        else:
+            raise TokenError
+    except TokenError:
+        logger.critical("Отсутствует PRACTICUM_TOKEN! РАБОТА ПРЕКРАЩЕНА")
+
+    try:
+        if TELEGRAM_TOKEN:
+            telegram_token_correct = True
+        else:
+            raise TokenError
+    except TokenError:
+        logger.critical("Отсутствует TELEGRAM_TOKEN! РАБОТА ПРЕКРАЩЕНА")
+
+    try:
+        if TELEGRAM_CHAT_ID:
+            telegram_chat_id_correct = True
+        else:
+            raise TokenError
+    except TokenError:
+        logger.critical("Отсутствует TELEGRAM_CHAT_ID! РАБОТА ПРЕКРАЩЕНА")
+
+    if (practicum_token_correct
+            and telegram_token_correct
+            and telegram_chat_id_correct):
+        return True
+    return False
 
 
 def main():
-    """Основная логика работы бота."""
+    """Основная логика работы бота.
 
-    ...
+    Последовательность действий:
+        Запрос к API.
+        Проверка ответа.
+        Если есть обновления — получает статус работы из обновления и
+        отправить сообщение в Telegram.
+        Ждет некоторое время и делает новый запрос.
+    """
+    if not check_tokens():
+        sys.exit()
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
 
-    ...
+    status_error = 0
+    token_error = 0
+    api_error = 0
 
     while True:
         try:
-            response = ...
+            response = get_api_answer(current_timestamp)
 
-            ...
+            check = check_response(response)
+            if check:
+                status_homework = parse_status(check[:-1][0])
+                send_message(bot, status_homework)
+                logger.info("Сообщение с новым статусом отправлено")
 
-            current_timestamp = ...
+            current_timestamp = response["current_date"]
             time.sleep(RETRY_TIME)
 
+        except ParseNoneStatus as error_status:
+            message = (
+                f"Сбой в работе, недокументированный статус домашней "
+                f"работы, обнаруженный в ответе API: {error_status}"
+            )
+            logger.error(message)
+            if status_error == 0:
+                send_message(bot, message)
+                logger.info("Отправка ошибки ParseNoneStatus")
+                status_error += 1
+            elif status_error > 10:
+                status_error = 0
+            time.sleep(RETRY_TIME)
+        except TokenError as error_token:
+            message = f"Отсутствие ожидаемых ключей от API: {error_token}"
+            logger.error(message)
+            if token_error == 0:
+                send_message(bot, message)
+                logger.info("Отправка ошибки TokenError")
+                token_error += 1
+            elif status_error > 10:
+                token_error = 0
+            time.sleep(RETRY_TIME)
+        except ApiError as error_api:
+            message = f"Нет доступа к API: {error_api}"
+            logger.error(message)
+            if api_error == 0:
+                send_message(bot, message)
+                logger.info("Отправка ошибки ApiError")
+                api_error += 1
+            elif api_error > 10:
+                api_error = 0
+            time.sleep(RETRY_TIME)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
+            message = f"Сбой в работе программы: {error}"
+            logger.error(message)
+            send_message(bot, message)
+            logger.info("Отправка непредвиденной ошибки")
             time.sleep(RETRY_TIME)
         else:
-            ...
+            logger.debug("В ответе нет изменений")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
